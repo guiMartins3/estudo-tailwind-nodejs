2ce8523345c3b95f1c2813f82b6d0d7c418a1f89
Guilherme Leandro <guilherme.lmartins33@gmail.com>
2026-03-09 17:38:35 -0300

    feat: implementa lógica para adicionar itens ao carrinho
    - Captura clique no botão de adicionar ao carrinho
    - Obtém nome e preço via data attributes
    - Verifica se o item já existe no carrinho
    - Incrementa quantidade ou adiciona novo item
    - Corrige seleção de elemento do DOM (span alterado para div)

--- Projeto_Cardapio/script.js
+++ Projeto_Cardapio/script.js
@@ -5,7 +5,7 @@ const cartItemsContainer = document.getElementById("cart-items")
 const cartTotal = document.getElementById("cart-total")
 const checkoutBtn = document.getElementById("checkout-btn")
 const closeModalBtn = document.getElementById("close-modal-btn")
-const cartCounter = document.getElementById("cart-counter")
+const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
 const addressWarning = document.getElementById("address-warn")
 
@@ -27,3 +27,36 @@ cartModal.addEventListener("click", function(event){
 closeModalBtn.addEventListener("click", function(){
     cartModal.style.display = "none"
 })
+
+// Adicionar ao carrinho
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn")
+    
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        // Adicionar no carrinho
+        addToCart(name, price)
+    }
+})
+
+
+// Função para adicionar no carrinho
+function addToCart(name, price){
+    // Verificar se o item já existe no carrinho
+    const existingItem = cart.find(item => item.name === name)
+
+    //Se o item já existir, aumenta a quantidade
+    if(existingItem){
+        existingItem.quantity += 1
+    } else{
+        // Vai adicionar no carrinho o nome, preço e quantidade
+        cart.push({
+            name,
+            price,
+            quantity: 1
+        })
+    }
+
+}
